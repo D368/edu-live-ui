9ad5f88d41d6c8b8dbf58497e88d4fae06a06a34
liangw <liangwei@tr-ag.com>
2020-09-04 17:28:24 +0800

    33

--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,16 @@ module.exports = {
                     '^/api': ''
                 }
             }
+            // '/hls': {
+            //     // http://121.36.228.230:8080/hls/test.m3u8
+            //     target: 'http://121.36.228.230:8080',
+            //     // 允许跨域
+            //     changeOrigin: true,
+            //     ws: true,
+            //     pathRewrite: {
+            //         '^/hls': '/hls'
+            //     }
+            // }
         }
     }
 }
